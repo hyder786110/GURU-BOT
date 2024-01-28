73d5f25ef69c5ac8c33c0ce1667b64aed85c53c5
hyder786110 <157951688+hyder786110@users.noreply.github.com>
2024-01-28 21:16:24 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923118284388', 'Hyder Bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
